8e2559c6021eba89652da860e7356c053a2c4170
SnezhanaKorsak <snezhana_korsak@mail.ru>
2023-01-09 22:39:13 +0300

    React.lazy and change favicon.ico

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
-import NotFound from './components/pages/NotFound';
-import Cart from './components/pages/Cart';
-import FullPizza from './components/pages/FullPizza';
 import Layout from './Layout';
+
+const Cart = React.lazy(() => import('./components/pages/Cart'));
+const NotFound = React.lazy(() => import('./components/pages/NotFound'));
+const FullPizza = React.lazy(() => import('./components/pages/FullPizza'));
 
 import './scss/app.scss';
 
@@ -15,9 +16,30 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route path='' element={<Home />} />
-        <Route path='cart' element={<Cart />} />
-        <Route path='pizza/:id' element={<FullPizza />} />
-        <Route path='*' element={<NotFound />} />
+        <Route
+          path='cart'
+          element={
+            <Suspense fallback={null}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path='pizza/:id'
+          element={
+            <Suspense fallback={null}>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <Suspense fallback={null}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
